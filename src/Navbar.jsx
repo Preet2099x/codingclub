a8011eb179cc2cab7e545ee7f983c88f90066ca0
Preet2099x <preet2099x@gmail.com>
2024-04-24 13:40:40 +0530

    added navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  const [count, setCount] = useState(0);
+
+  const handleSignUp = () => {
+    // Functionality for sign up
+    console.log('Sign Up clicked');
+  };
+
+  const handleLogin = () => {
+    // Functionality for login
+    console.log('Login clicked');
+  };
+
+  return (
+    <div id='mainNavbar'>
+      <div id='leftElements'>
+        <img src="./src/assets/navlogo.png" alt="image" />
+        <p>Qezee</p>
+      </div>
+      <div id='rightElements'>
+        <button onClick={handleLogin}>LOGIN</button>
+        <button onClick={handleSignUp}>SIGN-UP</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Signup from './SignUp'
+import Navbar from './Navbar'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Navbar/>
     <Signup/>
   </React.StrictMode>,
 )
